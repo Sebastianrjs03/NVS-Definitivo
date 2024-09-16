cc2d78b7d5613c2af124087e7dd227424fae96d2
Iku2326 <nicolasrami2006@gmail.com>
2024-09-16 12:08:32 -0500

    rutas soporte(si de nuevo pq bachelot daño todo)

--- admin/puntos_cliente/historial-puntos.php
+++ admin/puntos_cliente/historial-puntos.php
@@ -162,7 +162,9 @@ $resultado_Factura = $sql_Factura->fetchAll(PDO::FETCH_ASSOC);
                                 </label>
                                 <input type="checkbox" id="soporte">
                                 <ul>
+                                    <a href="../soporte/mod_soporte.php">
                                     <li style="font-size: 12px; margin-bottom: 1px;">PQRS</li>
+                                    </a>
                                 </ul>
                             </li>
                         </ul>
